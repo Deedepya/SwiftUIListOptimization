4119abf6f8a108e84feb682f4ad7944cf31bc75a
DedeepyaRSalla <146378612+DedeepyaRSalla@users.noreply.github.com>
2025-07-15 19:55:46 -0500

    Created swiftui ios app

---
+++ SwiftUIListExample/SwiftUIListExample/SwiftUIListExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListExampleApp.swift
+//  SwiftUIListExample
+//
+//  Created by Dedeepya Salla on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
